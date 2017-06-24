ee21111972798070a8787a50dbbe7630fe771db5
bstadt <brandonduderstadt@gmail.com>
2017-06-24 14:25:35 -0400

    continued work on autolabel

--- pre-processing/autoLabel/autoLabel.py
+++ pre-processing/autoLabel/autoLabel.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import numpy as np
 
-from skimage.exposure import equalize_adapthist as clahe
+from skimage.measure import label
 
 #becuse opencv requires a callback for trackbars
 def nothing(x):
@@ -25,7 +25,27 @@ def get_next_label(frame, hMin, sMin, vMin, hMax, sMax, vMax):
                                 np.array([hMin, sMin, vMin]),
                                 np.array([hMax, sMax, vMax]))
 
-    return threshedFrame
+    ero = threshedFrame
+    for i in range(5):
+        ero = cv2.erode(ero, (5, 5), 1)
+    dil = ero
+    for i in range(7):
+        dil = cv2.dilate(dil, (5, 5), 1)
+
+    labelImg = label(dil)
+
+    maxArea = 0
+    maxLabel = 0
+
+    #assume 0 label is background
+    for i in range(1, np.max(labelImg)):
+        area = len(zip(*np.where(labelImg == i)))
+        if area > maxArea:
+            maxArea = area
+            maxLabel = i
+
+    return dil
+
 
 if __name__ == '__main__':
     cap = open_capture(ignore=[0])
@@ -38,11 +58,11 @@ if __name__ == '__main__':
 
     cv2.namedWindow('Thresh')
     cv2.createTrackbar('HMin', 'Thresh', 0, 255, nothing)
-    cv2.createTrackbar('SMin', 'Thresh', 0, 255, nothing)
-    cv2.createTrackbar('VMin', 'Thresh', 0, 255, nothing)
-    cv2.createTrackbar('HMax', 'Thresh', 0, 255, nothing)
-    cv2.createTrackbar('SMax', 'Thresh', 0, 255, nothing)
-    cv2.createTrackbar('VMax', 'Thresh', 0, 255, nothing)
+    cv2.createTrackbar('SMin', 'Thresh', 16, 255, nothing)
+    cv2.createTrackbar('VMin', 'Thresh', 18, 255, nothing)
+    cv2.createTrackbar('HMax', 'Thresh', 48, 255, nothing)
+    cv2.createTrackbar('SMax', 'Thresh', 150, 255, nothing)
+    cv2.createTrackbar('VMax', 'Thresh', 79, 255, nothing)
 
     while 1:
         flag, frame = cap.read()
